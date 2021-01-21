4f25a3cd623df57b43a82a528cceb1cbcdc69add
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-01-20 17:32:38 -0800

    connected api database && updated Days with api/days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
+
+import axios from "axios";
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -60,26 +62,16 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get('api/days')
+    .then(response => {
+      setDays(response.data)
+    })
+  }, [])
 
   return (
     <main className="layout">
@@ -93,8 +85,8 @@ export default function Application(props) {
           <nav className="sidebar__menu">
             <DayList
               days={days}
-              day={day}
-              setDay={setDay}
+              day={days}
+              setDay={setDays}
             />
           </nav>
           <img className="sidebar__lhl sidebar--centered"
